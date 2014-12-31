ba23c92b7fd7486f486209145de765f45676ca13
Madan <dmadan86@gmail.com>
2014-12-31 13:33:38 +0530

    - Added examples

--- src/jsonp.js
+++ src/jsonp.js
@@ -1,70 +1,66 @@
-(function(){
+;(function (window, document) {
+    'use strict';
     var encode = window.encodeURIComponent,
-        emptyFn = function(){},
-        object_to_uri = function(obj) {
+        emptyFn = function () {},
+        src,
+        object_to_uri = function (obj) {
             var data = [], key;
             for (key in obj) {
-                data.push(encode(key) + '=' + encode(obj[key]));
+                if (obj.hasOwnProperty(key)) {
+                    data.push(encode(key) + '=' + encode(obj[key]));
+                }
             }
             return data.join('&');
         },
-        JSONP = function(options) {
-            var document = window.document,
-                src=[],
-                callback,
-                isLoaded = false,
+        JSONP = function (options) {
+            var isLoaded = false,
                 head = document.getElementsByTagName('head')[0],
                 params = {
                     data: options.data || {},
                     error: options.error || emptyFn,
                     success: options.success || emptyFn,
                     url: options.url || '',
-                    callback: options.callback || ("jsonp_" + (new Date()).getTime())
+                    callback: options.callback || ("jsonp_" + (+new Date()))
                 },
-                script= document.createElement('script');
-            options = options ? options : {};
+                script = document.createElement('script');
             if (params.url.length === 0) {
                 throw new Error('No URL Provided...');
             }
-            callback = params.callback;
-            window[callback] = function(data) {
-                setTimeout(function(){params.success(data);},0);
+            var callback = params.callback;
+            params.data.callback = callback;
+            window[callback] = function (data) {
+                setTimeout(function () {
+                    params.success(data);
+                }, 0);
                 return delete window[callback];
             };
             src = [
                 params.url,
-                params.url.indexOf('?' === -1) ? '?' : '&',
+                params.url.indexOf('?') === -1 ? '?' : '&',
                 object_to_uri(params.data)
             ];
-            script.src = src.join("");
             script.async = true;
-            script.onerror = function(evt) {
+            script.onerror = function (evt) {
                 return params.error({
                     url: script.src,
                     event: evt
                 });
             };
-            script.onload = script.onreadystatechange = function() {
+            script.onload = script.onreadystatechange = function () {
                 if (!isLoaded && (!this.readyState || this.readyState === 'loaded' || this.readyState === 'complete')) {
                     isLoaded = true;
                     script.onload = script.onreadystatechange = null;
                     if (script && script.parentNode) {
-                        return script.parentNode.removeChild(script);
+                        script.parentNode.removeChild(script);
                     }
                 }
             };
+            script.src = src.join("");
             return head.appendChild(script);
         };
 
+    window.JSONP = JSONP;
 
-    if ((typeof module !== "undefined" && module !== null) && module.exports) {
-        module.exports = JSONP;
-    } else if ((typeof module !== "undefined" && module !== null) && module.exports) {
-        module.exports = JSONP;
-    } else {
-        this.JSONP = JSONP;
-    }
-
-}).call(this);
+})(window, document);
 
 
